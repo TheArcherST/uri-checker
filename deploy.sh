43d528f34d6c3ba5d4d9d19fb83fd7d19de62a9b
Mihail Shapovalov <mihailsapovalov05@gmail.com>
2024-07-31 15:07:50 +0300

    Fixup deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
-docker compose up --build
+docker compose up --build -d
 docker compose build taskiq-worker
-docker stack deploy uri-discover
+docker compose push taskiq-worker &
+docker stack deploy -c swarm-stack.yml uri-discover
 docker compose push taskiq-worker
